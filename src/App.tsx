97774fc09525ae092585a85e6720c4af7f3c2da1
Kat <katrinaburwash_17@hotmail.com>
2023-03-07 11:44:21 +0100

    started styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <div className="main">
+        <div>
+          <h5>Add a new category</h5>
+          <input type="text" placeholder="New category"/>
+        </div>
+
+        <div className="add-habit-div">
+          <h5> Add habit</h5>
+          <input type="text" placeholder="Select habit"/>
+          <input type="text" placeholder="Date" />
+          <button type="submit" className="submit-btn">Add</button>
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header-flex">
+        <div>
+          <h1>My Habits</h1>
+          <p className="slogan">
+            - Turning habits into routine, one day at a time -
+          </p>
+        </div>
+        <div>
+          <p>Menu</p>
+        </div>
+      </div>
+    </header>
+  );
+}
